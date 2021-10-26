57bd6070c070153d9cc02e1942ff7de16f338850
Julian Gründner <juliangruendner@googlemail.com>
2021-10-26 20:39:16 +0200

    fix problem with certificate extension loading for opal

--- add_cert_to_opal_and_restart.sh
+++ add_cert_to_opal_and_restart.sh
@@ -2,7 +2,7 @@ export COMPOSE_PROJECT=ds-qp
 
 docker exec datashield_opal bash -c "cd /opt/opal/bin && ./import_opal_cert.sh"
 
-cd ds_opal/deploy
+cd ds_opal
 
 docker-compose -p $COMPOSE_PROJECT stop
 docker-compose -p $COMPOSE_PROJECT up -d
--- cert/create_dev_ca.sh
+++ cert/create_dev_ca.sh
@@ -1,4 +1,4 @@
 echo "Generating Certificat Authority and certificates for development"
 
 
-openssl req -config cacert.cnf -extensions v3_ca -days 3650 -new -x509 -keyout ./ca_cert/ds_develop_ca.key -out ./ca_cert/ds_develop_ca.crt -nodes
+openssl req -config cacert.cnf -days 3650 -new -x509 -keyout ./ca_cert/ds_develop_ca.key -out ./ca_cert/ds_develop_ca.crt -nodes
